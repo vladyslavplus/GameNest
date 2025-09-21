0289357090e492e00897c1d18f54ebcf160e5960
vladyslavplus <vladislavperevispa12345@gmail.com>
2025-09-21 12:43:39 +0300

    feat: add custom exceptions

---
+++ src/GameNest.OrderService.Domain/Exceptions/BusinessConflictException.cs
@@ -0,0 +1,7 @@
+﻿namespace GameNest.OrderService.Domain.Exceptions
+{
+    public class BusinessConflictException : Exception
+    {
+        public BusinessConflictException(string message) : base(message) { }
+    }
+}
---
+++ src/GameNest.OrderService.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace GameNest.OrderService.Domain.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message) { }
+    }
+}
